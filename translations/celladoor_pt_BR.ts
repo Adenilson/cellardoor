9c9cf34d88954306328fb774135802b65c60c699
Igor Oliveira <igor.oliveira@openbossa.org>
2011-03-16 10:15:40 -0400

    CellaDoor/Translations: move to a better directory

---
+++ translations/celladoor_pt_BR.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR">
+<context>
+    <name>main</name>
+    <message>
+        <source>Welcome to</source>
+        <translatorcomment>Bem vindo ao</translatorcomment>
+        <translation type="unfinished">Bem vindo ao</translation>
+    </message>
+    <message>
+        <source>your personal wine journal</source>
+        <translation type="unfinished">seu diario pessoal de vinhos</translation>
+    </message>
+</context>
+</TS>
