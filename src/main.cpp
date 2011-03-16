3eabb26e90ed0038305991c10e67b668f08706c3
Igor Oliveira <igor.oliveira@openbossa.org>
2011-03-16 10:18:03 -0400

    CellaDoor: implement translation system to celladoor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <QtGui/QApplication>
+#include <QtCore/QLocale>
+#include <QtCore/QTranslator>
+#include <QDebug>
 #include "controller.h"
 #include "utils.h"
+
+static char *s_applicationName = "celladoor";
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     //QApplication::setGraphicsSystem("opengl");
+    app.setApplicationName(s_applicationName);
+
+    QString locale = QLocale::system().name();
+    QTranslator translator;
+    //XXX: HACK
+#ifdef Q_OS_SYMBIAN
+    translator.load(app.applicationName() + "_" + QLocale::system().name());
+#else
+    translator.load("translations/" + app.applicationName() + "_" + QLocale::system().name());
+#endif
+    app.installTranslator(&translator);
 
     ReminderController controller;
     controller.initUI();
