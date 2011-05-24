66ea54000d919c8c32b279d07ea1244c68b98632
Adenilson Cavalcanti da Silva <adenilson.silva@openbossa.org>
2011-05-23 23:57:41 -0400

    Using the sound effect version for meego.

--- src/controller.cpp
+++ src/controller.cpp
@@ -24,8 +24,7 @@ void CellarController::initUI()
 
     QString tmp(m_sysInfo->model());
     //XXX: At least in mobility 1.2 beta1 in Symbian^3 it fails to load
-    //and in meego netbook 1.1 gstreamer will not play WAV
-    if (tmp.contains("i686"))
+    if (tmp.contains("i686") || tmp.contains("Atom"))
         m_view->setSource(QUrl("qrc:/qml/main.qml"));
     else {
         m_view->setSource(QUrl("qrc:/qml/mainNosound.qml"));
