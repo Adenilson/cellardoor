d73900698537816d267e0c3b1f03596ae336ba44
Adenilson Cavalcanti da Silva <adenilson.silva@openbossa.org>
2011-05-24 00:04:43 -0400

    If running in meego, show it fullscreen.

--- src/controller.cpp
+++ src/controller.cpp
@@ -24,10 +24,15 @@ void CellarController::initUI()
 
     QString tmp(m_sysInfo->model());
     //XXX: At least in mobility 1.2 beta1 in Symbian^3 it fails to load
-    if (tmp.contains("i686") || tmp.contains("Atom"))
+    if (tmp.contains("i686"))
         m_view->setSource(QUrl("qrc:/qml/main.qml"));
     else {
-        m_view->setSource(QUrl("qrc:/qml/mainNosound.qml"));
+        if (tmp.contains("Atom")) {
+            m_view->setSource(QUrl("qrc:/qml/main.qml"));
+        } else {
+            m_view->setSource(QUrl("qrc:/qml/mainNosound.qml"));
+        }
+
         m_view->showFullScreen();
     }
 }
