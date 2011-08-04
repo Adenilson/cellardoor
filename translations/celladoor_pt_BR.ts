ca907aa666134209055dd038c7deb2513d1287bd
Adenilson Cavalcanti da Silva <adenilson.silva@openbossa.org>
2011-08-03 20:42:03 -0400

    Updating the translation file (It is being a while since I last edited it).

--- translations/celladoor_pt_BR.ts
+++ translations/celladoor_pt_BR.ts
@@ -2,13 +2,15 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="pt_BR">
 <context>
-    <name>main</name>
+    <name>Splash</name>
     <message>
+        <location filename="Splash.qml" line="82"/>
         <source>Welcome to</source>
         <translatorcomment>Bem vindo ao</translatorcomment>
         <translation type="unfinished">Bem vindo ao</translation>
     </message>
     <message>
+        <location filename="Splash.qml" line="121"/>
         <source>your personal wine journal</source>
         <translation type="unfinished">seu diario pessoal de vinhos</translation>
     </message>
