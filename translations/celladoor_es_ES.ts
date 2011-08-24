82843a7de33aa28a4db0a6c0ce3d7895ba302798
Adenilson Cavalcanti da Silva <adenilson.silva@openbossa.org>
2011-08-24 10:30:04 -0400

    Adding Spanish translation. Contribution by Ignacio Poggi.
    Signed-off-by: Adenilson Cavalcanti da Silva <adenilson.silva@openbossa.org>

---
+++ translations/celladoor_es_ES.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>Splash</name>
+    <message>
+        <location filename="Splash.qml" line="82"/>
+        <source>Welcome to</source>
+        <translation>Bienvenido a</translation>
+    </message>
+    <message>
+        <location filename="Splash.qml" line="121"/>
+        <source>your personal wine journal</source>
+        <translation>Su agenda personal de vinos</translation>
+    </message>
+</context>
+<context>
+    <name>SplashNoSound</name>
+    <message>
+        <location filename="SplashNoSound.qml" line="77"/>
+        <source>Welcome to</source>
+        <translation>Bienvenido a</translation>
+    </message>
+    <message>
+        <location filename="SplashNoSound.qml" line="114"/>
+        <source>your personal wine journal</source>
+        <translation>Su agenda personal de vinos</translation>
+    </message>
+</context>
+<context>
+    <name>View</name>
+    <message>
+        <location filename="View.qml" line="63"/>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="View.qml" line="186"/>
+        <source>add new wine</source>
+        <translation>Añadir vino</translation>
+    </message>
+    <message>
+        <location filename="View.qml" line="238"/>
+        <source>Holly Grapes! You haven&apos;t tasted a</source>
+        <translation>¡Santas uvas! No ha degustado un</translation>
+    </message>
+    <message>
+        <location filename="View.qml" line="239"/>
+        <source>single wine, let&apos;s fix it!</source>
+        <translation>solo vino, repararlo!</translation>
+    </message>
+    <message>
+        <location filename="View.qml" line="241"/>
+        <location filename="View.qml" line="244"/>
+        <source>You have tasted</source>
+        <translation>Ha degustado</translation>
+    </message>
+    <message>
+        <location filename="View.qml" line="242"/>
+        <source>wine</source>
+        <translation>vino</translation>
+    </message>
+    <message>
+        <location filename="View.qml" line="245"/>
+        <source>wines</source>
+        <translation>vinos</translation>
+    </message>
+</context>
+<context>
+    <name>Input</name>
+    <message>
+        <location filename="Input.qml" line="56"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="Input.qml" line="75"/>
+        <source>Back</source>
+        <translation>Atrás</translation>
+    </message>
+    <message>
+        <location filename="Input.qml" line="99"/>
+        <source>Adding a new wine to your Cellar</source>
+        <translation>Añadiendo un vino a su bodega</translation>
+    </message>
+    <message>
+        <location filename="Input.qml" line="118"/>
+        <location filename="Input.qml" line="202"/>
+        <source>Step 1</source>
+        <translation>Paso 1</translation>
+    </message>
+    <message>
+        <location filename="Input.qml" line="153"/>
+        <location filename="Input.qml" line="197"/>
+        <location filename="Input.qml" line="226"/>
+        <source>Next</source>
+        <translation>Siguiente</translation>
+    </message>
+    <message>
+        <location filename="Input.qml" line="231"/>
+        <source>Step 2</source>
+        <translation>Paso 2</translation>
+    </message>
+    <message>
+        <location filename="Input.qml" line="255"/>
+        <source>Finish</source>
+        <translation>Finalizar</translation>
+    </message>
+    <message>
+        <location filename="Input.qml" line="260"/>
+        <source>Step 3</source>
+        <translation>Paso 3</translation>
+    </message>
+</context>
+<context>
+    <name>CheckBar</name>
+    <message>
+        <location filename="CheckBar.qml" line="29"/>
+        <source>Red</source>
+        <translation>Tinto</translation>
+    </message>
+    <message>
+        <location filename="CheckBar.qml" line="39"/>
+        <source>White</source>
+        <translation>Blanco</translation>
+    </message>
+    <message>
+        <location filename="CheckBar.qml" line="50"/>
+        <source>Other</source>
+        <translation>Otro</translation>
+    </message>
+</context>
+<context>
+    <name>Delegate</name>
+    <message>
+        <location filename="Delegate.qml" line="118"/>
+        <source>Producer</source>
+        <translation>Productor</translation>
+    </message>
+    <message>
+        <location filename="Delegate.qml" line="128"/>
+        <source>Region</source>
+        <translation>Región</translation>
+    </message>
+</context>
+<context>
+    <name>InputOne</name>
+    <message>
+        <location filename="InputOne.qml" line="44"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="45"/>
+        <location filename="InputOne.qml" line="53"/>
+        <source>Wine name here</source>
+        <translation>Nombre del vino</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="54"/>
+        <source>red</source>
+        <translation>tinto</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="55"/>
+        <source>Grape type e.g Merlot</source>
+        <translation>Tipo de uva ej. Merlot</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="56"/>
+        <location filename="InputOne.qml" line="90"/>
+        <source>Vineyard</source>
+        <translation>Viñedo</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="58"/>
+        <source>$bucks</source>
+        <translation>$precio</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="89"/>
+        <source>Producer</source>
+        <translation>Productor</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="99"/>
+        <source>Vintage Year</source>
+        <translation>Año</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="111"/>
+        <source>Price</source>
+        <translation>Precio</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="124"/>
+        <source>Kind of Wine</source>
+        <translation>Tipo de vino</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="140"/>
+        <source>Color</source>
+        <translation>Color</translation>
+    </message>
+    <message>
+        <location filename="InputOne.qml" line="141"/>
+        <source>wine color</source>
+        <translation>color del vino</translation>
+    </message>
+</context>
+<context>
+    <name>InputSecond</name>
+    <message>
+        <location filename="InputSecond.qml" line="44"/>
+        <source>Tasting comments</source>
+        <translation>Comentarios de la degustación</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="45"/>
+        <location filename="InputSecond.qml" line="53"/>
+        <source>How it tastes here</source>
+        <translation type="unfinished">¿Qué gusto tiene?</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="54"/>
+        <location filename="InputSecond.qml" line="79"/>
+        <source>How it compare</source>
+        <translation>¿A qué se compara?</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="55"/>
+        <location filename="InputSecond.qml" line="89"/>
+        <source>Your current location</source>
+        <translation>Su ubicación actual</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="57"/>
+        <location filename="InputSecond.qml" line="99"/>
+        <source>1 to 5</source>
+        <translation>de 1 a 5</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="78"/>
+        <source>Comparisons</source>
+        <translation>Comparaciones</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="88"/>
+        <source>Place</source>
+        <translation>Lugar</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="98"/>
+        <source>Rating</source>
+        <translation>Puntuación</translation>
+    </message>
+    <message>
+        <location filename="InputSecond.qml" line="110"/>
+        <source>Date consumed</source>
+        <translation>Fecha de consumición</translation>
+    </message>
+</context>
+</TS>
